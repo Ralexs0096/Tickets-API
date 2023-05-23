97ac174c7fd1d3d3e1eef49e2f20ffddd45a7103
Alexs Ruiz <ralexs.acu@gmail.com>
2023-05-23 12:44:06 -0600

    feat(routes): basic route configuration for:
    - Areas
    - Tickets
    - Users

---
+++ src/routes/users/index.ts
@@ -0,0 +1,25 @@
+import { FastifyPluginCallback, RouteOptions } from 'fastify';
+
+/** Imports Routes */
+
+/** Define Public Routes */
+const routesForAuthServer = [] as RouteOptions[];
+
+export const authRoutes: FastifyPluginCallback = (server, _opts, next) => {
+  for (const route of routesForAuthServer) {
+    server.route(route);
+  }
+  next();
+};
+
+/** Define Private Routes */
+const routerForPublicServer = [] as RouteOptions[];
+
+export const publicRoute: FastifyPluginCallback = (server, _opts, next) => {
+  for (const route of routerForPublicServer) {
+    server.route(route);
+  }
+  next();
+};
+
+export default { authRoutes, publicRoute };
