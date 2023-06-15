8fd0d4cafbadef5ef65b1a12978bc8a6d2213d8d
Alexs Ruiz <ralexs.acu@gmail.com>
2023-06-14 23:29:46 -0600

    feat(area): update area handler

--- src/routes/areas/updateArea.ts
+++ src/routes/areas/updateArea.ts
@@ -9,6 +9,7 @@ import AreaSchema from '../../schemas/Area.json';
 import { AreaRequestParams } from '../../types/AreaRequestParams';
 import AreaRequestParamsSchema from '../../schemas/AreaRequestParams.json';
 import { Area } from '../../types/Area';
+import AreaModel from '../../models/area';
 
 type Reply = {};
 type UpdateAreaRoute = {
@@ -19,8 +20,28 @@ type UpdateAreaRoute = {
 
 const url = '/area/:id';
 
-const handler: RouteHandler<UpdateAreaRoute> = (req, reply) => {
-  console.log(`Area with ID ${req.params.id} will be updated`);
+const handler: RouteHandler<UpdateAreaRoute> = async (req, reply) => {
+  const idArea = req.params.id;
+  const newAreaName = req.body.name;
+
+  const areaResponse = await AreaModel.query().findById(idArea);
+
+  if (!areaResponse) {
+    return reply.status(404).send({
+      message: 'This area does not exist'
+    });
+  }
+
+  if (
+    areaResponse.name.toLocaleLowerCase() === newAreaName.toLocaleLowerCase()
+  ) {
+    return reply.status(204).send();
+  }
+
+  await AreaModel.query().updateAndFetchById(idArea, {
+    name: newAreaName.toUpperCase()
+  });
+
   reply.status(204).send();
 };
 
@@ -36,7 +57,7 @@ const schema = {
       title: 'InvalidArea',
       description: 'Invalid or missing Area data.',
       type: 'object',
-      required: ['error'],
+      required: ['name'],
       properties: {
         error: {}
       }
