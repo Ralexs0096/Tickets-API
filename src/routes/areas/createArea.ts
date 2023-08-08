5fd7804e8b45e0aacb244c58bd5fe2e35af09746
Alexs Ruiz <ralexs.acu@gmail.com>
2023-08-07 21:52:34 -0600

    feat(createArea): create multiple areas

--- src/routes/areas/createArea.ts
+++ src/routes/areas/createArea.ts
@@ -5,42 +5,47 @@ import {
   RouteHandler,
   RouteOptions
 } from 'fastify';
-import AreaSchema from '../../schemas/Area.json';
 import { Area } from '../../types/Area';
+import CreateAreaSchema from '../../schemas/CreateArea.json';
 import AreaModel from '../../models/area';
+import { CreateArea } from '../../types/CreateArea';
 
 type Reply = Area | { error: {} };
 type CreateAreaRoute = {
-  Body: Area;
+  Body: CreateArea;
   Reply: Reply;
 };
 
 const url = '/area';
 
 export const handler: RouteHandler<CreateAreaRoute> = async (req, reply) => {
-  const createdArea = await AreaModel.query().insert({
-    // we will register all Areas in Capital Letters
-    name: req.body.name.toUpperCase(),
+  const newAreas = req.body.map((area) => {
+    return {
+      // we will register all Areas in Capital Letters
+      name: area.name?.toLocaleUpperCase(),
 
-    /** TODO: update these values with requester USER */
-    CreatedBy: 'ADMIN_TEST',
-    ModifiedBy: 'ADMIN_TEST',
-    CreatedDate: new Date(),
-    ModifiedDate: new Date()
+      /** TODO: update these values with requester USER */
+      CreatedBy: 'ADMIN_TEST',
+      ModifiedBy: 'ADMIN_TEST',
+      CreatedDate: new Date(),
+      ModifiedDate: new Date()
+    };
   });
 
-  reply.status(201).send({
-    name: createdArea.name
-  });
+  await AreaModel.query().insert(newAreas);
+
+  reply.status(200).send();
 };
 
 export const schema = {
   operationId: 'createArea',
   tags: ['Area'],
   summary: 'Create a new Area.',
-  body: AreaSchema,
+  body: CreateAreaSchema,
   response: {
-    201: AreaSchema,
+    200: {
+      type: 'null'
+    },
     400: {
       title: 'InvalidArea',
       description: 'Invalid or missing Area data.',
