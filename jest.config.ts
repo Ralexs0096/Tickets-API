c37181cb4e0960ad946c33b69f73d764ef38e298
Alexs Ruiz <ralexs.acu@gmail.com>
2023-09-09 23:22:06 -0600

    test(jestConfig): add the necessary configuration for testing using jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  testTimeout: 30000,
+  projects: [
+    {
+      displayName: 'test',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      globalSetup: '<rootDir>/src/utils/createDatabase.ts',
+      clearMocks: true,
+      resetMocks: true,
+      restoreMocks: true,
+      setupFilesAfterEnv: ['<rootDir>/src/utils/setupTests.ts'],
+      testPathIgnorePatterns: ['/node_modules/', '<rootDir>/build/'],
+      modulePathIgnorePatterns: ['<rootDir>/build/'],
+      moduleFileExtensions: ['ts', 'json', 'js']
+    }
+  ]
+};
