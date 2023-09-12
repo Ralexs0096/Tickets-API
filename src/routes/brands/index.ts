b6e427793b32d4babc4ff45e737a5694f9119b39
Leandro Matus <leandromatus85@gmail.com>
2023-09-12 14:38:22 -0600

    feat(routes): add get Brands endpoint.
    -This returns an array of objects with the names of the Brands.

--- src/routes/brands/index.ts
+++ src/routes/brands/index.ts
@@ -2,11 +2,15 @@ import { FastifyPluginCallback, RouteOptions } from "fastify";
 import createBrand from "./createBrand";
 import updateBrand from "./updateBrand";
 import deleteBrand from "./deleteBrand";
+import fetchAllBrands from "./fetchAllBrands";
 
 const routesForAuthServer = [
   createBrand,
   updateBrand,
   deleteBrand
+  updateBrand,
+  fetchAllBrands
+
 ] as RouteOptions[];
 
 export const authRoutes: FastifyPluginCallback = (server, _opts, next) => {
