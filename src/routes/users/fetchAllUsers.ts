cc19b67194ee9e2fc8de3c2e06e3d0f8a44a6ff4
Leandro Matus <leandromatus85@gmail.com>
2023-10-20 14:00:45 -0600

    feat(users/fetchAllUsers): add ErrorSchema response type

--- src/routes/users/fetchAllUsers.ts
+++ src/routes/users/fetchAllUsers.ts
@@ -7,33 +7,33 @@ import {
 } from "fastify";
 import { User } from "../../types/User";
 import UserModel from "../../models/user";
+import { ErrorSchema } from "../../types/ErrorSchema";
+import ErrorSchemaJson from "../../schemas/ErrorSchema.json";
+import UserSchema from "../../schemas/User.json"
 
 type FetchAllUsers = {
-  Reply: User[] | { error: { code: string; message: string } };
+  Reply: User[] | ErrorSchema;
 };
 
 const url = "/user";
 
 export const handler: RouteHandler<FetchAllUsers> = async (req, reply) => {
   try {
-    const users = await UserModel.query().select("firstName", "lastName","areaId");
-    if (users.length > 0) {
-      reply.status(200).send(users);
-    } else {
-      reply.status(404).send({
-        error: {
-          code: "unknown",
-          message: "No users found.",
-        },
-      });
+    const users = await UserModel.query().select(
+      "firstName",
+      "lastName",
+      "areaId"
+    );
+    if (users.length === 0) {
+      return reply.status(204).send();
     }
+    reply.status(200).send(users);
   } catch (error) {
     return reply.status(500).send({
-        error: {
-          code: "unknown",
-          message: `An unknown error occurred when trying to fetch users. Error: ${error}`,
-        },
-      });
+      error: `${error}`,
+      statusCode: 500,
+      message: "An unknown error occurred when trying to fetch users." ,
+    });
   }
 };
 
@@ -43,62 +43,12 @@ export const schema = {
   summary: "Fetch All Users",
   response: {
     200: {
-      title: "User",
+      title: "Users",
       type: "array",
-      required: ["firstName ", "lastName","areaId"],
-      additionalProperties: false,
-      properties: {
-        firstName: {
-          type: "string",
-        },
-        lastName: {
-            type: "string",
-          },
-          areaId: {
-            type: "number" || "null",
-          },
-      },
+      propiesties:UserSchema
     },
-    404: {
-      title: "InvalidUser",
-      description: "Invalid or missing User data.",
-      type: "object",
-      required: ["error"],
-      properties: {
-        error: {
-          type: "object",
-          required: ["code", "message"],
-          properties: {
-            code: {
-              type: "string",
-            },
-            message: {
-              type: "string",
-            },
-          },
-        },
-      },
-    },
-    500: {
-      title: "Error",
-      description: "An unknown error occurred when trying to fetch users.",
-      type: "object",
-      required: ["error"],
-      properties: {
-        error: {
-          type: "object",
-          required: ["code", "message"],
-          properties: {
-            code: {
-              type: "string",
-            },
-            message: {
-              type: "string",
-            },
-          },
-        },
-      },
-    },
+    204: {},
+    500: ErrorSchemaJson,
   },
 };
 
