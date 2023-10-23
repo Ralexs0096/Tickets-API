6b6234f08721f634e82f850f9cf8137ce2e9f17f
Alexs Ruiz <ralexs.acu@gmail.com>
2023-10-22 23:30:45 -0600

    test(createArea): add a first case for area creation

--- src/routes/areas/createArea.test.ts
+++ src/routes/areas/createArea.test.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, RouteOptions } from 'fastify';
 import createServer from '../../server';
 import createArea from './createArea';
+import AreaModel from '../../models/area';
 
 const url = '/area';
 
@@ -10,19 +11,27 @@ describe('POST /areas', () => {
     server = createServer({ authRoute: [createArea] as RouteOptions[] });
   });
 
-  it('returns a 400 status code', async () => {
+  it('returns a 500 status code when try to create an area that already exist', async () => {
+    await AreaModel.query().insert({
+      name: 'TEST',
+      CreatedBy: 'ADMIN_TEST',
+      ModifiedBy: 'ADMIN_TEST',
+      CreatedDate: new Date(),
+      ModifiedDate: new Date()
+    });
+
     const response = await server.inject({
       method: 'POST',
       url,
-      payload: [
-        {
-          name: 'Test'
-        }
-      ]
+      payload: {
+        areas: [
+          {
+            name: 'Test'
+          }
+        ]
+      }
     });
 
-    console.log({ response });
-
-    expect(response.statusCode).toEqual(400);
+    expect(response.statusCode).toEqual(500);
   });
 });
