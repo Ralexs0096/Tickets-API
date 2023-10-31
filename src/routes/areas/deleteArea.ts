9f104eff0a5ea009db633c42b7e04fe4c0cf13db
Leandro Matus <leandromatus85@gmail.com>
2023-10-31 17:51:50 -0600

    feat(areas/deleteArea): add the try-catch and add ErrorSchema to deleteArea

--- src/routes/areas/deleteArea.ts
+++ src/routes/areas/deleteArea.ts
@@ -23,22 +23,32 @@ type DeleteAreaRoute = {
 const url = "/area/:id";
 
 const handler: RouteHandler<DeleteAreaRoute> = async (req, reply) => {
-  const { id: areaId } = req.params;
-  const areaResponse = await AreaModel.query().findById(areaId);
+  try {
+    const { id: areaId } = req.params;
+    const areaResponse = await AreaModel.query().findById(areaId);
 
-  if (!areaResponse) {
-    return reply.status(404).send({
+    if (!areaResponse) {
+      return reply.status(404).send({
+        error: {
+          error: "Not Found",
+          code: "NotFound",
+          message: "This area does not exist",
+        },
+      });
+    }
+
+    await AreaModel.query().deleteById(areaId);
+
+    return reply.status(204).send();
+  } catch (error) {
+    return reply.status(500).send({
       error: {
-        error: "Not Found",
-        code: "NotFound",
-        message: "This area does not exist",
+        error: `${error}`,
+        code: "Unknown",
+        message: "An unknown error occurred when trying to delete an area.",
       },
     });
   }
-
-  await AreaModel.query().deleteById(areaId);
-
-  return reply.status(204).send();
 };
 
 const schema = {
@@ -52,6 +62,15 @@ const schema = {
     404: {
       title: "InvalidArea",
       description: "Invalid or missing Area data.",
+      type: "object",
+      require: ["error"],
+      properties: {
+        error: ErrorSchemaJson,
+      },
+    },
+    500: {
+      title: "Error",
+      description: "An unknown error occurred when trying to delete areas.",
       type: "object",
       require: ["error"],
       properties: {
