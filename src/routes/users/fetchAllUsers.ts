ddc718105fa0e7f0959a10dcddaf60526439ffce
Leandro Matus <leandromatus85@gmail.com>
2023-11-09 19:32:39 -0600

    feat(users/fetchAllUsers): add ErrorSchema to fetchAllUsers

--- src/routes/users/fetchAllUsers.ts
+++ src/routes/users/fetchAllUsers.ts
@@ -9,30 +9,35 @@ import { User } from "../../types/User";
 import UserModel from "../../models/user";
 import { ErrorSchema } from "../../types/ErrorSchema";
 import ErrorSchemaJson from "../../schemas/ErrorSchema.json";
-import UserSchema from "../../schemas/User.json"
+import UserSchema from "../../schemas/User.json";
 
 type FetchAllUsers = {
-  Reply: User[] | ErrorSchema;
+  Reply: User[] | { error: ErrorSchema };
 };
 
 const url = "/user";
 
 export const handler: RouteHandler<FetchAllUsers> = async (req, reply) => {
   try {
-    const users = await UserModel.query().select(
-      "firstName",
-      "lastName",
-      "areaId"
-    );
+    const users = await UserModel.query();
     if (users.length === 0) {
-      return reply.status(204).send();
+      return reply.status(404).send({
+        error: {
+          error: "Not Found",
+          code: "NotFound",
+          message: "There are not Areas currently",
+        },
+      });
     }
+
     reply.status(200).send(users);
   } catch (error) {
     return reply.status(500).send({
-      error: `${error}`,
-      statusCode: 500,
-      message: "An unknown error occurred when trying to fetch users." ,
+      error: {
+        error: `${error}`,
+        code: "Unknown",
+        message: "An unknown error occurred when trying to fetch users.",
+      },
     });
   }
 };
@@ -45,10 +50,27 @@ export const schema = {
     200: {
       title: "Users",
       type: "array",
-      propiesties:UserSchema
+      propiesties: UserSchema,
     },
-    204: {},
-    500: ErrorSchemaJson,
+    404: {
+      title: "Not found",
+      description: "Invalid or missing Brand data.",
+      type: "object",
+      require: ["error"],
+      properties: {
+        error: ErrorSchemaJson,
+      },
+    },
+    500: {
+      title: "Error",
+      description:
+        "An unknown error occurred while attempting to retrieve users data.",
+      type: "object",
+      require: ["error"],
+      properties: {
+        error: ErrorSchemaJson,
+      },
+    },
   },
 };
 
