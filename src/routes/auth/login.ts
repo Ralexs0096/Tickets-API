e40d561041317fd7267451090c26ee79b5beb4e2
Alex Ruiz <ralexs.acu@gmail.com>
2025-11-08 16:37:16 -0600

    fix(login): add reply type to LoginRoute and adjust the endpoint url

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -6,24 +6,27 @@ import {
   RouteOptions,
 } from 'fastify';
 import LoginRequestSchema from '../../schemas/Body/LoginRequest.json';
-import { LoginRequest as LoginRequestBody } from './../../types/Body/LoginRequest.d';
+import { LoginRequest as LoginRequestBody } from './../../types/Body/LoginRequest';
+import { LoginReply } from '../../types/Reply/LoginReply';
+import { WithError } from '../../utils/typesUtilities';
 
 interface LoginRoute {
   Body: LoginRequestBody;
+  Reply: WithError<LoginReply>;
 }
 
-const url = '/area';
+const url = '/login';
 
 export const handler: RouteHandler<LoginRoute> = async (req, reply) => {
   try {
-    return reply.status(201).send({});
+    const { email, password } = req.body;
+
+    console.log(email, password);
   } catch (error) {
     return reply.status(500).send({
-      error: {
-        error: `${error}`,
-        code: 'Unknown',
-        message: 'An unknown error occurred when trying to fetch areas.',
-      },
+      error: `${error}`,
+      code: 'Unknown',
+      message: 'An unknown error occurred when trying to fetch areas.',
     });
   }
 };
