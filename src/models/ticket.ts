51c4e80ba4ddde931b4b76faa0651d040f3f3f80
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-06 23:40:55 -0600

    feat(ticket): adjust the model to handle the correct type and relation mappings

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -1,4 +1,7 @@
+import { Model } from 'objection';
 import AuditModel from './auditModel';
+import Style from './style';
+import Brand from './brand';
 
 class Ticket extends AuditModel {
   static get tableName() {
@@ -9,10 +12,31 @@ class Ticket extends AuditModel {
   declare cutNumber: number;
 
   /** can be fetched using relation "styleModel" */
-  declare style?: string;
+  declare style?: Style;
 
   /** can be fetched using relation "brandModel" */
-  declare brand?: string;
+  declare brand?: Brand;
+
+  static get relationMappings() {
+    return {
+      style: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Style,
+        join: {
+          from: 'tickets.styleId',
+          to: 'styles.id',
+        },
+      },
+      brand: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Brand,
+        join: {
+          from: 'tickets.brandId',
+          to: 'brands.id',
+        },
+      },
+    };
+  }
 }
 
 export default Ticket;
