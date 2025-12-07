f4280caf99f8c37fa27e5cd76c7e101c2c77e8be
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-07 16:23:49 -0600

    feat(fetchAllTickets): implement query param filters to allow filtering by date, ticket number and provide a limit on each request

--- src/routes/tickets/fetchAllTickets.ts
+++ src/routes/tickets/fetchAllTickets.ts
@@ -6,32 +6,57 @@ import {
   RouteOptions,
 } from 'fastify';
 
-import TicketModel from '../../models/ticket';
-
 import NotFoundSchema from '../../schemas/NotFound.json';
 import InternalServerErrorSchema from '../../schemas/InternalError.json';
 import FetchAllTicketsReplySchema from '../../schemas/Reply/FetchAllTicketsReply.json';
+import FetchAllTicketsQueryStringSchema from '../../schemas/QueryStrings/FetchAllTicketsQueryString.json';
+
 import { WithError } from '../../utils/typesUtilities';
 import { FetchAllTicketsReply } from '../../types/Reply/FetchAllTicketsReply';
+import { FetchAllTicketsQueryString } from './../../types/QueryStrings/FetchAllTicketsQueryString.d';
+import { buildTicketBaseQuery } from './helpers/buildTicketBaseQuery';
 
 interface FetchAllTickets {
   Reply: WithError<FetchAllTicketsReply>;
+  Querystring: FetchAllTicketsQueryString;
 }
 
 const url = '/tickets';
 
-export const handler: RouteHandler<FetchAllTickets> = async (_, reply) => {
+export const handler: RouteHandler<FetchAllTickets> = async (req, reply) => {
   try {
-    const result = (await TicketModel.query()
-      .joinRelated('[style, brand]')
-      .select(
-        'tickets.id',
-        'tickets.cutNumber',
-        'brand.name as brand',
-        'style.code as style'
+    const { limit = 5, date, ticketNumber } = req.query;
+
+    if (ticketNumber) {
+      const requestedTicket = (await buildTicketBaseQuery()
+        .where('cutNumber', ticketNumber)
+        .first()) as unknown as FetchAllTicketsReply['tickets'][1];
+
+      return reply.status(200).send({
+        tickets: requestedTicket ? [requestedTicket] : [],
+      });
+    }
+
+    const targetDate = date || new Date().toISOString().slice(0, 10); // YYYY-MM-DD
+
+    const todaysTickets = (await buildTicketBaseQuery()
+      .whereRaw('CONVERT(date, tickets.CreatedDate) = ?', [targetDate])
+      .orderBy(
+        'tickets.CreatedDate',
+        'desc'
       )) as unknown as FetchAllTicketsReply['tickets'];
 
-    if (result.length === 0) {
+    if (todaysTickets.length > 0) {
+      return reply.status(200).send({
+        tickets: todaysTickets,
+      });
+    }
+
+    const lastTickets = (await buildTicketBaseQuery()
+      .orderBy('tickets.CreatedDate', 'desc')
+      .limit(limit)) as unknown as FetchAllTicketsReply['tickets'];
+
+    if (lastTickets.length === 0) {
       return reply.status(404).send({
         error: {
           error: 'Not Found',
@@ -42,7 +67,7 @@ export const handler: RouteHandler<FetchAllTickets> = async (_, reply) => {
     }
 
     return reply.status(200).send({
-      tickets: result,
+      tickets: lastTickets,
     });
   } catch (error) {
     // TODO: implement a log system more robust
@@ -68,6 +93,7 @@ export const schema = {
   operationId: 'fetchAllTickets',
   tags: ['Ticket'],
   summary: 'Fetch All existing tickets',
+  querystring: FetchAllTicketsQueryStringSchema,
   response: {
     200: FetchAllTicketsReplySchema,
     404: NotFoundSchema,
