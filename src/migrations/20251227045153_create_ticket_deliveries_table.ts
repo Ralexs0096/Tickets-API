7ff03b974ee311086670decf8259cb0b6f553465
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-26 23:05:51 -0600

    feat(migrations): add new migration to add new ticket_deliveries table and relationships

---
+++ src/migrations/20251227045153_create_ticket_deliveries_table.ts
@@ -0,0 +1,26 @@
+import { Knex } from 'knex';
+import Migration, { fixTable } from '../utils/Migrations';
+
+export async function up(knex: Knex): Promise<void> {
+  await Migration.createTableIfNotExists(knex, 'ticket_deliveries', (table) => {
+    table = fixTable(table);
+
+    table.increments('id').primary();
+    table.integer('ticket_id').unsigned().notNullable();
+    table.integer('area_id').unsigned().notNullable();
+    table.integer('user_id').unsigned().notNullable();
+    table.timestamp('delivered_at').notNullable();
+    table.string('notes').nullable();
+
+    table.foreign('ticket_id').references('id').inTable('tickets');
+    table.foreign('area_id').references('id').inTable('areas');
+    table.foreign('user_id').references('id').inTable('users');
+  });
+
+  const ticketDeliveriesMigration = new Migration(knex, 'ticket_deliveries');
+  await ticketDeliveriesMigration.addAuditColumns();
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('ticket_deliveries');
+}
