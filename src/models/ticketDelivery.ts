1ed2e86d3d5e70e910a4bcfa3b9a6a6a68a62485
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-26 23:42:32 -0600

    feat(ticketDelivery): add relationship mappings for area, ticket and user

--- src/models/ticketDelivery.ts
+++ src/models/ticketDelivery.ts
@@ -1,4 +1,8 @@
+import { Model } from 'objection';
 import AuditModel from './auditModel';
+import Ticket from './ticket';
+import Area from './area';
+import User from './user';
 
 class TicketDelivery extends AuditModel {
   static get tableName() {
@@ -11,6 +15,39 @@ class TicketDelivery extends AuditModel {
   declare user_id: number;
   declare delivered_at: Date;
   declare notes: string | null;
+
+  declare ticket?: Ticket;
+  declare area?: Area;
+  declare user?: User;
+
+  static get relationMappings() {
+    return {
+      ticket: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Ticket,
+        join: {
+          from: 'ticket_deliveries.ticket_id',
+          to: 'tickets.id',
+        },
+      },
+      area: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Area,
+        join: {
+          from: 'ticket_deliveries.area_id',
+          to: 'areas.id',
+        },
+      },
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'ticket_deliveries.user_id',
+          to: 'users.id',
+        },
+      },
+    };
+  }
 }
 
 export default TicketDelivery;
